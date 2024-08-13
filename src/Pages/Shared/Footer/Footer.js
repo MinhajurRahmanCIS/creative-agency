62492799074aecc8ae070d293c2c0700734dc5d4
MinhajurRahmanCIS <rahamannovel@gmail.com>
2024-08-13 19:38:12 +0600

    accodion added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,45 @@
+import { Box, Button, Container, Divider, Typography } from '@mui/material';
 import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <Box sx={{
+            bgcolor:"primary.main",
+            mt:10,
+            p: 8
+        }}>
+            <Container>
+                <Box sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center"
+                }}>
+                    <Typography sx={{
+                        fontSize: "1.5rem",
+                        color: "#fff"
+                    }}>Creative Agency</Typography>
+                    <Box sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center"
+                }}>
+                    <Typography sx={{
+                        color: "rgba(255, 255, 255, 0.5)"
+                    }}>Creative Agency</Typography>
+                    <Button sx={{
+                        bgcolor: "primary.green",
+                        marginLeft: 2
+                    }}>Get Started</Button>
+                    </Box>
+                </Box>
+                <Divider 
+                sx={{
+                    mt: 3,
+                    bgcolor: "rgba(255, 255, 255, 0.5)"
+                }}
+                />
+            </Container>
+        </Box>
     );
 };
 
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -2,6 +2,7 @@ import { Box, Container } from '@mui/material';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
+import Footer from '../Pages/Shared/Footer/Footer';
 
 const Main = () => {
     return (
@@ -10,6 +11,7 @@ const Main = () => {
                 <Navbar/>
                 <Outlet />
             </Container>
+            <Footer/>
         </Box>
     );
 };
